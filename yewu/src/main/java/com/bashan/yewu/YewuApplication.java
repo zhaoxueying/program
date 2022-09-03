40d05832691554edc570e93d4987c67b599993d6
zhaoxueying <1462059721@qq.com>
2022-09-03 14:43:47 +0800

    第一次提交测试项目

---
+++ yewu/src/main/java/com/bashan/yewu/YewuApplication.java
@@ -0,0 +1,13 @@
+package com.bashan.yewu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YewuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YewuApplication.class, args);
+	}
+
+}
---
+++ yewu/src/test/java/com/bashan/yewu/YewuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bashan.yewu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YewuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
